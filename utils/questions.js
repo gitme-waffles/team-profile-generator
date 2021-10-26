89af638d636d5efbdb2e37dba75419b446eb8a6f
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-10-27 00:28:54 +1100

    questions list done

--- utils/questions.js
+++ utils/questions.js
@@ -1,17 +1,93 @@
-module.exports = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your Github username?',
-    },
-]
+const manager = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Assign an ID number",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office number?",
+  },
+];
+
+const engineer = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Assign an ID number",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?",
+  },
+];
+
+const intern = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Assign an ID number",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What Educational Institute did you attend?",
+  },
+];
+
+const employeeOptions = [
+  {
+    type: "list",
+    name: "selections",
+    message: "Add members to the team!",
+    choices: ["Engineer", "Intern", "Done"],
+  },
+];
+
+const confirm = [
+  {
+    type: "confirm",
+    name: "confirm",
+    message: "Are you sure you are done?",
+  },
+];
+
+module.exports = {
+  manager,
+  engineer,
+  intern,
+  employeeOptions,
+  confirm,
+};
